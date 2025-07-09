4f5384defcf8ad9b1322b0c8dfc56afd2e88d2bd
wjlee0908 <dev.woojin.lee@gmail.com>
2025-07-09 13:29:49 +0900

    feature: (header) modify styles

--- src/components/category-header.tsx
+++ src/components/category-header.tsx
@@ -1,7 +1,7 @@
 export function CategoryHeader() {
   return (
-    <header className="w-full h-14 bg-white border-gray-200 border-b-[1px] z-30">
-      <h1 className="text-2xl font-bold">생활</h1>
+    <header className="w-full h-14 py-4 bg-white border-gray-200 border-b-[1px] z-30">
+      <h1 className="text-base font-medium text-center">생활</h1>
     </header>
   );
 }
