23e7cb97be4ab6ab74550a6656253a64a6c97111
Woojin Lee <dev.woojin.lee@gmail.com>
2025-07-10 21:22:29 +0900

    feature: add subcategory tab

--- src/components/subcategory-tab.tsx
+++ src/components/subcategory-tab.tsx
@@ -1,3 +1,18 @@
-export function SubcategoryTab() {
-  return <div>SubcategoryTab</div>;
+export interface SubcategoryTabProps {
+  subcategories: string[];
+}
+
+export function SubcategoryTab({ subcategories }: SubcategoryTabProps) {
+  return (
+    <div className="flex gap-2 border-b border-gray-200">
+      {subcategories.map((subcategory) => (
+        <button
+          key={subcategory}
+          className="p-3 text-sm text-gray-400 cursor-pointer"
+        >
+          {subcategory}
+        </button>
+      ))}
+    </div>
+  );
 }
