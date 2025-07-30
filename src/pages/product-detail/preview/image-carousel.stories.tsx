e589299c8ca0775b4d6338abb3ab92f42178aba1
wjlee0908 <dev.woojin.lee@gmail.com>
2025-07-30 13:00:45 +0900

    feature: (product-detail) add prop to image carousel

--- src/pages/product-detail/preview/image-carousel.stories.tsx
+++ src/pages/product-detail/preview/image-carousel.stories.tsx
@@ -7,4 +7,11 @@ const meta = {
 
 export default meta;
 
-export const ImageCarouselStory: StoryObj<typeof meta> = {};
+export const ImageCarouselStory: StoryObj<typeof meta> = {
+  args: {
+    images: Array.from({ length: 5 }).map((_, index) => ({
+      src: "https://dummyimage.com/960x960.png",
+      alt: `Image ${index}`,
+    })),
+  },
+};
--- src/pages/product-detail/ui/image-carousel.tsx
+++ src/pages/product-detail/ui/image-carousel.tsx
@@ -2,15 +2,22 @@ import { CarouselPagination } from "@/widgets/carousel/ui/carousel";
 import { Carousel, CarouselContent, CarouselItem } from "@widgets/carousel";
 import Image from "next/image";
 
-export const ImageCarousel = () => {
+export interface ImageCarouselProps {
+  images: {
+    src: string;
+    alt: string;
+  }[];
+}
+
+export const ImageCarousel = ({ images }: ImageCarouselProps) => {
   return (
     <Carousel className="w-full max-w-xs flex flex-col gap-4">
       <CarouselContent>
-        {Array.from({ length: 5 }).map((_, index) => (
+        {images.map((image, index) => (
           <CarouselItem key={index} className="pl-0">
             <Image
-              src={`https://dummyimage.com/960x960.png`}
-              alt="Image"
+              src={image.src}
+              alt={image.alt}
               width={960}
               height={960}
               className="w-full h-full object-cover"
