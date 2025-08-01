eaa899183401bbd3bf5a1c5e9ad8aebe4db5d269
wjlee0908 <dev.woojin.lee@gmail.com>
2025-08-01 16:11:32 +0900

    feature: (header) change header fixed

--- src/pages/product-detail/ui/navigation.tsx
+++ src/pages/product-detail/ui/navigation.tsx
@@ -17,11 +17,6 @@ export enum NavItem {
 export const Navigation = ({ className }: { className?: string }) => {
   const [selected, setSelected] = useState<NavItem>(NavItem.DETAIL);
 
-  const scrollHeader = useScrollHeader();
-  const originTop = useInView({
-    rootMargin: `${-1 * HEADER_HEIGHT}px 0px 0px`,
-  });
-
   const navItems = [
     { id: NavItem.DETAIL, href: "#detail", label: "상세 정보" },
     { id: NavItem.REVIEW, href: "#review", label: "상품 리뷰" },
@@ -30,44 +25,40 @@ export const Navigation = ({ className }: { className?: string }) => {
   ];
 
   return (
-    <div className={className}>
-      <div ref={originTop.ref} className="h-0" />
-      <ContentWrapper
-        className={clsx(
-          "w-full px-4 bg-white",
-          !originTop.inView &&
-            "fixed top-0 left-0 transition-transform duration-300 ease-linear",
-          !originTop.inView && scrollHeader.isVisible && `translate-y-[52px]`
-        )}
+    <ContentWrapper
+      className={clsx(
+        "w-full px-4 bg-white",
+        "sticky top-[45px] left-0 transition-transform duration-300 ease-linear",
+        className
+      )}
+    >
+      <nav
+        className={
+          "relative flex items-center bg-white border-b border-gray-200"
+        }
       >
-        <nav
-          className={
-            "relative flex items-center bg-white border-b border-gray-200"
-          }
-        >
-          {navItems.map(({ id, href, label }) => (
-            <Link
-              className={clsx(
-                "flex-1 text-xs h-10 flex justify-center items-center",
-                selected === id && " font-semibold"
-              )}
-              key={href}
-              href={href}
-              onClick={() => setSelected(id)}
-            >
-              <p>{label}</p>
-            </Link>
-          ))}
-          <div
-            data-testid="slider"
-            className={`absolute bottom-0 left-0 border-b-2 border-black mb-[-1px] transition-all duration-300`}
-            style={{
-              width: `${100 / navItems.length}%`,
-              transform: `translateX(${selected * 100}%)`,
-            }}
-          />
-        </nav>
-      </ContentWrapper>
-    </div>
+        {navItems.map(({ id, href, label }) => (
+          <Link
+            className={clsx(
+              "flex-1 text-xs h-10 flex justify-center items-center",
+              selected === id && " font-semibold"
+            )}
+            key={href}
+            href={href}
+            onClick={() => setSelected(id)}
+          >
+            <p>{label}</p>
+          </Link>
+        ))}
+        <div
+          data-testid="slider"
+          className={`absolute bottom-0 left-0 border-b-2 border-black mb-[-1px] transition-all duration-300`}
+          style={{
+            width: `${100 / navItems.length}%`,
+            transform: `translateX(${selected * 100}%)`,
+          }}
+        />
+      </nav>
+    </ContentWrapper>
   );
 };
--- src/widgets/header/ui/header-view.tsx
+++ src/widgets/header/ui/header-view.tsx
@@ -14,25 +14,22 @@ export interface HeaderViewProps {
 export function HeaderView({ title }: HeaderViewProps) {
   const router = useRouter();
 
-  const scrollHeader = useScrollHeader();
-
   return (
     <header
       className={clsx(
-        `flex items-center justify-between w-full h-[${HEADER_HEIGHT}px] bg-white border-gray-200 border-b-[1px] sticky top-0 z-header transition-transform duration-300 ease-linear`,
-        !scrollHeader.isVisible && "translate-y-[-100%]"
+        `flex items-center justify-between w-full h-[${HEADER_HEIGHT}px] bg-white border-gray-200 border-b-[1px] sticky top-0 z-header transition-transform duration-300 ease-linear`
       )}
     >
       <button
         onClick={() => router.back()}
-        className="px-4 py-4 cursor-pointer"
+        className="px-3 py-3 cursor-pointer"
       >
         <ChevronLeftIcon className="text-black w-5 h-5" />
       </button>
       {title ? (
         <h1 className="text-base font-medium text-center">{title}</h1>
       ) : null}
-      <button className="px-4 py-4 cursor-pointer">
+      <button className="px-3 py-3 cursor-pointer">
         <ShoppingCartIcon className="text-black w-5 h-5 " />
       </button>
     </header>
