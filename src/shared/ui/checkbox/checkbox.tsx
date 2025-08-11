b5789c5389019d8c174d7f8948065d4c98482179
wjlee0908 <dev.woojin.lee@gmail.com>
2025-08-11 18:41:20 +0900

    feature: (login) add animation to checkbox

--- src/shared/ui/checkbox/checkbox.tsx
+++ src/shared/ui/checkbox/checkbox.tsx
@@ -1,32 +1,101 @@
 "use client";
 
 import * as React from "react";
-import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
-import { CheckIcon } from "lucide-react";
+import { Checkbox as CheckboxPrimitive } from "@base-ui-components/react/checkbox";
+import { type HTMLMotionProps, motion } from "motion/react";
 
 import { cn } from "@shared/lib/class-name";
 
-function Checkbox({
+type CheckboxProps = Omit<
+  React.ComponentProps<typeof CheckboxPrimitive.Root>,
+  "render"
+> & {
+  motionProps?: HTMLMotionProps<"button">;
+};
+
+export function Checkbox({
   className,
+  onCheckedChange,
+  motionProps,
   ...props
-}: React.ComponentProps<typeof CheckboxPrimitive.Root>) {
+}: CheckboxProps) {
+  const [isChecked, setIsChecked] = React.useState(
+    props?.checked ?? props?.defaultChecked ?? false
+  );
+
+  React.useEffect(() => {
+    if (props?.checked !== undefined) setIsChecked(props.checked);
+  }, [props?.checked]);
+
+  const handleCheckedChange = React.useCallback(
+    (checked: boolean, event: Event) => {
+      setIsChecked(checked);
+      onCheckedChange?.(checked, event);
+    },
+    [onCheckedChange]
+  );
+
   return (
     <CheckboxPrimitive.Root
       data-slot="checkbox"
       className={cn(
-        "peer border-input dark:bg-input/30 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground dark:data-[state=checked]:bg-primary data-[state=checked]:border-primary focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive size-4 shrink-0 rounded-[4px] border shadow-xs transition-shadow outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
+        "peer shrink-0 flex items-center justify-center outline-none focus-visible:ring-[3px] focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 disabled:cursor-not-allowed disabled:opacity-50 transition-colors duration-500",
+        "size-5 rounded-sm bg-white border border-input focus-visible:ring-offset-2 data-[checked]:border-primary data-[checked]:bg-primary data-[checked]:text-primary-foreground",
         className
       )}
       {...props}
+      onCheckedChange={handleCheckedChange}
+      render={
+        <motion.button
+          whileTap={{ scale: 0.95 }}
+          whileHover={{ scale: 1.05 }}
+          {...motionProps}
+        />
+      }
     >
       <CheckboxPrimitive.Indicator
+        keepMounted
         data-slot="checkbox-indicator"
         className="flex items-center justify-center text-current transition-none"
       >
-        <CheckIcon className="size-3.5" />
+        <motion.svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="3.5"
+          stroke="currentColor"
+          className="size-3.5"
+          initial="unchecked"
+          animate={isChecked ? "checked" : "unchecked"}
+        >
+          <motion.path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M4.5 12.75l6 6 9-13.5"
+            variants={{
+              checked: {
+                pathLength: 1,
+                opacity: 1,
+                transition: {
+                  duration: 0.2,
+                  delay: 0.2,
+                },
+              },
+              unchecked: {
+                pathLength: 0,
+                opacity: 0,
+                transition: {
+                  duration: 0.2,
+                },
+              },
+            }}
+          />
+        </motion.svg>
       </CheckboxPrimitive.Indicator>
     </CheckboxPrimitive.Root>
   );
 }
 
-export { Checkbox };
+Checkbox.displayName = CheckboxPrimitive.Root.displayName;
+
+export { Checkbox, type CheckboxProps };
