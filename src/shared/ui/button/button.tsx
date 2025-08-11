efe067c3724ba023bb5888f22f2276bdbbea9653
wjlee0908 <dev.woojin.lee@gmail.com>
2025-08-11 18:57:35 +0900

    feature: (button) add animation

--- src/shared/ui/button/button.tsx
+++ src/shared/ui/button/button.tsx
@@ -1,8 +1,10 @@
+"use client";
+
 import * as React from "react";
-import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@shared/lib/class-name";
+import { HTMLMotionProps, motion } from "motion/react";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
@@ -36,22 +38,16 @@ const buttonVariants = cva(
   }
 );
 
-function Button({
-  className,
-  variant,
-  size,
-  asChild = false,
-  ...props
-}: React.ComponentProps<"button"> &
-  VariantProps<typeof buttonVariants> & {
-    asChild?: boolean;
-  }) {
-  const Comp = asChild ? Slot : "button";
+type ButtonProps = HTMLMotionProps<"button"> &
+  VariantProps<typeof buttonVariants>;
 
+function Button({ className, variant, size, ...props }: ButtonProps) {
   return (
-    <Comp
+    <motion.button
       data-slot="button"
       className={cn(buttonVariants({ variant, size, className }))}
+      whileTap={{ scale: 0.95 }}
+      whileHover={{ scale: 1.05 }}
       {...props}
     />
   );
